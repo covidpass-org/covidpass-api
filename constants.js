6f27481e2da3ac13a9c3ec662923347bb86356bd
Khaos Tian <khaos.tian@gmail.com>
2021-06-27 17:28:26 -0700

    Support signing manifest

--- constants.js
+++ constants.js
@@ -1,9 +1,3 @@
-exports.BASE_URL = 'https://raw.githubusercontent.com/ehn-dcc-development/ehn-dcc-valuesets/main/'
-exports.VALUE_TYPES = {
-  medicalProducts: 'vaccine-medicinal-product.json',
-  countryCodes: 'country-2-codes.json',
-  manufacturers: 'vaccine-mah-manf.json',
-}
 exports.PASS_MIME_TYPE = 'application/vnd.apple.pkpass'
 exports.SECRETS = {
   TEAM_ID: process.env.TEAM_ID,
@@ -11,15 +5,5 @@ exports.SECRETS = {
   CERT: Buffer.from(
     typeof process.env.CERT === 'string' ? process.env.CERT : '', 'base64').toString('ascii'),
   PASSPHRASE: process.env.PASSPHRASE,
-}
-exports.COLORS = {
-  white: 'rgb(255, 255, 255)',
-  black: 'rgb(0, 0, 0)',
-  grey: 'rgb(33, 33, 33)',
-  green: 'rgb(27, 94, 32)',
-  indigo: 'rgb(26, 35, 126)',
-  blue: 'rgb(1, 87, 155)',
-  purple: 'rgb(74, 20, 140)',
-  teal: 'rgb(0, 77, 64)',
 }
 exports.NAME = 'CovidPass'
